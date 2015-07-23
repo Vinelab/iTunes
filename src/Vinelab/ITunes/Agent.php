2dff6b87e6566ee98737638063cfe86fa08e5288
Abed Halawi <halawi.abed@gmail.com>
2015-07-23 14:17:10 +0300

    separate Agent and LaraveAgent
    this separation keeps the backward compatibility of using this library
    outside of Laravel without the requirement of a Cache manager.

--- src/Vinelab/ITunes/Agent.php
+++ src/Vinelab/ITunes/Agent.php
@@ -1,168 +1,114 @@
-<?php namespace Vinelab\ITunes;
+<?php
+
+namespace Vinelab\ITunes;
 
 use Illuminate\Config\Repository;
-use Illuminate\Cache\CacheManager;
 use Vinelab\Http\Client as HttpClient;
-
 use Vinelab\ITunes\Exceptions\ConfigurationException;
 use Vinelab\ITunes\Exceptions\InvalidSearchException;
 
-class Agent {
-
-    /**
-     * What to prefix the cache key with
-     * @var string
-     */
-    protected $cachePrefix = 'v:itunes';
-
+/**
+ * The iTunes agent.
+ *
+ * Responsible for performing the request to the iTunes API.
+ *
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ *
+ * @since 1.0.0
+ */
+class Agent
+{
     /**
      * @var Illuminate\Config\Repository
      */
-    protected $_Config;
-
-    /**
-     * @var Illuminate\Cache\CacheManager
-     */
-    protected $_Cache;
+    protected $config;
 
     /**
      * @var Vinelab\HttpClient
      */
-    protected $_HttpClient;
+    protected $http;
 
     /**
+     * The API Request Configuration.
      *
-     * The duration to cache the received results
-     * @var float
-     */
-    protected $defaultCacheDuration = 60;
-
-    /**
-     * The API Request Configuration
      * @var array
      */
-    protected $iTunesConfig;
+    protected $iTunesConfig = [];
 
     public function __construct(
         Repository $config = null,
-        CacheManager $cacheManager = null,
-        HttpClient $http = null )
-    {
-
-        $this->_Config = $config ?: new Repository;
-        $this->_Cache = $cacheManager ?: new CacheManager;
-        $this->_Http = $http;
-
+        HttpClient $http = null
+    ) {
+        $this->config = $config ?: new Repository();
+        $this->http = $http ?: new HttpClient();
         // load library configuration
-        $this->iTunesConfig = $this->_Config->get('itunes');
-
-        // Set the default cache duration (overridden by config if found)
-        if (!isset($this->iTunesConfig['cache'])) $this->iTunesConfig['cache'] = $this->defaultCacheDuration;
+        $this->loadConfig();
     }
 
     /**
-     * Search the API for a term
-     * @param  string $term
-     * @param  array  $params
+     * Search the API for a term.
+     *
+     * @param string $term
+     * @param array  $params
+     *
      * @return object
      */
     public function search($term, $params = array())
     {
-        $params = array_merge(compact('term'), $params);
+        $params = $this->getSearchParams($term, $params);
+        $results = $this->http->get($this->request('search', $params))->json();
 
-        $cacheKey = $this->cacheKey('search', $params);
-        $cacheDuration = $this->iTunesConfig['cache'];
-
-        if ($this->_Cache->has($cacheKey))
-        {
-            return $this->_Cache->get($cacheKey);
-        } else {
-
-            $results = $this->_Http->get($this->request('search', $params))->json();
-
-            if ($results)
-            {
-                return $this->_Cache->remember($cacheKey, $cacheDuration, function() use($results) {
-                    return json_encode($results);
-                });
-
-            } else {
-                return json_encode($this->emptyResults());
-            }
-        }
+        return $results ?: $this->emptyResults();
     }
 
     /**
-     * Search withing a defined region
-     * @param  string $region
-     * @param  string $term
-     * @param  array  $params
+     * Search withing a defined region.
+     *
+     * @param string $region
+     * @param string $term
+     * @param array  $params
+     *
      * @return obejct
      */
     public function searchRegion($region, $term, $params = array())
     {
-        return $this->search($term, array_merge(array('country'=>strtoupper($region)), $params));
+        return $this->search($term, array_merge(array('country' => strtoupper($region)), $params));
     }
 
     /**
-     * Lookup an item in the API
-     * @param  string $item
-     * @param  array  $params
+     * Lookup an item in the API.
+     *
+     * @param string $item
+     * @param array  $params
+     *
      * @return object
      */
     public function lookup($id, $value = null, $params = array())
     {
-        // Make the id default
-        if (!$value)
-        {
-            $value = $id;
-            $id = 'id';
-        }
+        $results = $this->http->get($this->request('lookup', $this->getLookupParams($id, $value, $params)))->json();
 
-        $params = array_merge(array($id=>$value), $params);
-
-        $cacheKey = $this->cacheKey('lookup', $params);
-        $cacheDuration = $this->iTunesConfig['cache'];
-
-        if ($this->_Cache->has($cacheKey))
-        {
-            return $this->_Cache->get($cacheKey);
-        } else {
-
-            $results = $this->_Http->get($this->request('lookup', $params))->json();
-
-            if ($results)
-            {
-                return $this->_Cache->remember($cacheKey, $cacheDuration, function() use($results) {
-                    return json_encode($results);
-                });
-
-            } else {
-                return json_encode($this->emptyResults());
-            }
-        }
+        return $results ?: $this->emptyResults();
     }
 
     /**
      * This method is automatically called through
      * the __call method. A convenience in order to be able
-     * to perform searches like:
+     * to perform searches like:.
      *
      * music($term); musicInRegion($region, $term);
      * tvShow($show); tvShowInregion($region, $show);
      *
-     * @param  string $method
-     * @param  array $arguments
+     * @param string $method
+     * @param array  $arguments
+     *
      * @return object
      */
     public function specificMediaSearch($method, $arguments)
     {
-        if (isset($arguments[0]))
-        {
+        if (isset($arguments[0])) {
             $params = array();
 
-            if (strpos($method, 'InRegion') !== false && isset($arguments[1]))
-            {
+            if (strpos($method, 'InRegion') !== false && isset($arguments[1])) {
                 // performing regional search
                 list($region, $term) = $arguments;
                 $params['country'] = strtoupper($region);
@@ -170,54 +116,49 @@ class Agent {
                 // remove InRegion to get the media type out of the method name
                 $media = str_replace('InRegion', '', $method);
                 $term = $arguments[1];
-
             } else {
-
                 $media = $method;
                 $term = $arguments[0];
             }
 
             $params['media'] = $media;
 
-            if (count($arguments) > 1 && is_array($arguments[count($arguments)-1]))
-            {
-                $params = array_merge($arguments[count($arguments)-1], $params);
+            if (count($arguments) > 1 && is_array($arguments[count($arguments) - 1])) {
+                $params = array_merge($arguments[count($arguments) - 1], $params);
             }
 
             return $this->search($term, $params);
         }
 
-        throw new InvalidSearchException("Missing arguments for search");
+        throw new InvalidSearchException('Missing arguments for search');
     }
 
     /**
-     * Builds up the request array
+     * Builds up the request array.
      *
-     * @param  string $type supported: search | lookup
-     * @param  string $term
-     * @param  array  $params
+     * @param string $type   supported: search | lookup
+     * @param string $term
+     * @param array  $params
+     *
      * @return array
      */
     public function request($type, $params = array())
     {
-        if (isset($this->iTunesConfig['api']))
-        {
+        if (isset($this->iTunesConfig['api'])) {
             if (
                 isset($this->iTunesConfig['api']['url']) &&
                 isset($this->iTunesConfig['api']['search_uri']) &&
                 isset($this->iTunesConfig['api']['lookup_uri'])
             ) {
-
                 $host = $this->iTunesConfig['api']['url'];
 
-                switch($type)
-                {
+                switch ($type) {
                     case 'search':
                     default:
                         $uri = $this->iTunesConfig['api']['search_uri'];
                         $params = array_merge(
                             array(
-                                'limit' => isset($this->iTunesConfig['limit']) ? $this->iTunesConfig['limit'] : 50
+                                'limit' => isset($this->iTunesConfig['limit']) ? $this->iTunesConfig['limit'] : 50,
                             ),
                             $params
                         );
@@ -229,8 +170,8 @@ class Agent {
                 }
 
                 return array(
-                    'url'    => $host.$uri,
-                    'params' => $params
+                    'url' => $host.$uri,
+                    'params' => $params,
                 );
             }
         }
@@ -238,25 +179,42 @@ class Agent {
         throw new ConfigurationException('Incomplete Configuration');
     }
 
-    public function cacheFor($minutes)
+    /**
+     * Get the query parameters for a search request.
+     *
+     * @param string $term
+     * @param array  $params
+     *
+     * @return array
+     */
+    protected function getSearchParams($term, array $params)
     {
-        return $this->iTunesConfig['cache'] = $minutes;
+        return array_merge(compact('term'), $params);
+    }
+
+    /**
+     * Get the query params for a lookup request.
+     *
+     * @param string $id
+     * @param string $value
+     * @param array  $params
+     *
+     * @return array
+     */
+    protected function getLookupParams($id, $value, array $params)
+    {
+        // Make the id default
+        if (!$value) {
+            $value = $id;
+            $id = 'id';
+        }
+
+        return array_merge(array($id => $value), $params);
     }
 
     protected function emptyResults()
     {
-        return array('resultsCount'=>0, 'results'=>array());
-    }
-
-    /**
-     * [cacheKey description]
-     * @param  string $term
-     * @param  array $params
-     * @return string
-     */
-    protected function cacheKey($type, $params)
-    {
-        return sprintf('%s:%s:%s', $this->cachePrefix, $type, md5(http_build_query($params)));
+        return array('resultsCount' => 0, 'results' => array());
     }
 
     public function __call($method, $arguments)
@@ -266,5 +224,18 @@ class Agent {
         // i.e. $agent->tvShow(...)
         // i.e. $agent->musicInRegion(...)
         return $this->specificMediaSearch($method, $arguments);
+    }
+
+    /**
+     * Load the configuration parameters.
+     *
+     * @return
+     */
+    protected function loadConfig()
+    {
+        // load the detaulf configuration.
+        $this->iTunesConfig = (array) require_once __DIR__.'/../../config/itunes.php';
+        // merge overriding with user-specified configuration.
+        $this->iTunesConfig = array_merge($this->iTunesConfig, $this->config->get('itunes'));
     }
 }
---
+++ src/Vinelab/ITunes/LaravelAgent.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Vinelab\ITunes;
+
+use Illuminate\Config\Repository;
+use Illuminate\Cache\CacheManager;
+use Vinelab\Http\Client as HttpClient;
+
+/**
+ * An Extension of the Agent class that adds caching capabilities.
+ *
+ * @author Abed Halawi <abed.halawi@vinelab.com>
+ *
+ * @since 1.2.0
+ */
+class LaravelAgent extends Agent
+{
+    /**
+     * What to prefix the cache key with.
+     *
+     * @var string
+     */
+    protected $cachePrefix = 'v:itunes';
+
+    /**
+     * The duration to cache the received results.
+     *
+     * @var int
+     */
+    protected $defaultCacheDuration = 60;
+
+    /**
+     * @var Illuminate\Cache\CacheManager
+     */
+    protected $cache;
+
+    public function __construct(
+        Repository $config = null,
+        CacheManager $cacheManager = null,
+        HttpClient $http = null
+    ) {
+        parent::__construct($config, $http);
+
+        $this->cache = $cacheManager;
+        // Set the default cache duration (overridden by config if found)
+        if (!isset($this->iTunesConfig['cache'])) {
+            $this->iTunesConfig['cache'] = $this->defaultCacheDuration;
+        }
+    }
+
+    public function cacheFor($minutes)
+    {
+        return $this->iTunesConfig['cache'] = $minutes;
+    }
+
+    /**
+     * Search the API for a term.
+     *
+     * @param string $term
+     * @param array  $params
+     *
+     * @return object
+     */
+    public function search($term, $params = array())
+    {
+        $params = $this->getSearchParams($term, $params);
+        $cacheKey = $this->cacheKey('search', $params);
+        $cacheDuration = $this->iTunesConfig['cache'];
+
+        if ($this->cache->has($cacheKey)) {
+            return $this->cache->get($cacheKey);
+        } else {
+            return $this->cache->remember($cacheKey, $cacheDuration, function () {
+                return json_encode(parent::search($term, $params));
+            });
+        }
+    }
+
+    /**
+     * Lookup an item in the API.
+     *
+     * @param string $item
+     * @param array  $params
+     *
+     * @return object
+     */
+    public function lookup($id, $value = null, $params = array())
+    {
+        $cacheKey = $this->cacheKey('lookup', $this->getLookupParams($id, $value, $params));
+        $cacheDuration = $this->iTunesConfig['cache'];
+
+        if ($this->cache->has($cacheKey)) {
+            return $this->cache->get($cacheKey);
+        } else {
+            return $this->cache->remember($cacheKey, $cacheDuration, function () {
+                return json_encode(parent::lookup($id, $value, $params));
+            });
+        }
+    }
+
+    /**
+     * get a cache key for the given type and params.
+     *
+     * @param string $term
+     * @param array  $params
+     *
+     * @return string
+     */
+    protected function cacheKey($type, $params)
+    {
+        return sprintf('%s:%s:%s', $this->cachePrefix, $type, md5(http_build_query($params)));
+    }
+}
